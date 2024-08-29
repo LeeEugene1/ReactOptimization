70d49e1b51d4a56ab72f140bd15a09c52c39ca90
LeeEugene1 <uwm1004@gmail.com>
2024-08-29 20:49:07 +0900

    memo메소드를 통한 todoItem, header의 불필요한 리렌더링 방지

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { memo } from "react";
 import "./Header.css";
+import { useEffect } from "react";
 
 const Header = () => {
+  useEffect(() => {
+    console.log("해더컴포넌트가 출력됨");
+  });
   return (
     <div className="Header">
       <h3>오늘은 📆</h3>
@@ -9,4 +14,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default memo(Header); //header컴포넌트가 불필요하게 렌더링되지않음
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,3 +1,4 @@
+import { memo } from "react";
 import "./TodoItem.css";
 
 const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
@@ -24,4 +25,13 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default TodoItem;
+export default memo(TodoItem, (prevProps, nextProps) => {
+  //반환값에 따라, props가 바뀌었는지 안바뀌었는지 판단
+  //T -> props가 바뀌지않았다 -> 리렌더링 x
+  //F -> props가 바뀌었다 -> 리렌더링
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+  return true;
+});
