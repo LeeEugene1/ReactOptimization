2a9f3627aa6cd75f31b58fba351b8b126e986caf
LeeEugene1 <uwm1004@gmail.com>
2024-08-29 21:00:25 +0900

    useCallback 불필요한 함수 리랜더링 방지

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -25,13 +25,17 @@ const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default memo(TodoItem, (prevProps, nextProps) => {
-  //반환값에 따라, props가 바뀌었는지 안바뀌었는지 판단
-  //T -> props가 바뀌지않았다 -> 리렌더링 x
-  //F -> props가 바뀌었다 -> 리렌더링
-  if (prevProps.id !== nextProps.id) return false;
-  if (prevProps.isDone !== nextProps.isDone) return false;
-  if (prevProps.content !== nextProps.content) return false;
-  if (prevProps.date !== nextProps.date) return false;
-  return true;
-});
+// //onUpdate, onDelete(객체함수는 주소값이 계속 다르게 저장되기때문에 얕은비교시 계속다름)
+// //때문에 계속 리렌더링되서 memo를 하려면 prev, next비교가 필요함
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   //반환값에 따라, props가 바뀌었는지 안바뀌었는지 판단
+//   //T -> props가 바뀌지않았다 -> 리렌더링 x
+//   //F -> props가 바뀌었다 -> 리렌더링
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+//   return true;
+// });
+
+export default memo(TodoItem);
